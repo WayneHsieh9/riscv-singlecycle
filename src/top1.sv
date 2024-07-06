64d01529044820f557bc2a3aeaae102b877fe1c8
Weichih Hsieh <hsieh81@ee65lnx39.ecn.purdue.edu>
2024-07-06 16:50:47 -0400

    initial commit Wayne

--- src/top1.sv
+++ src/top1.sv
@@ -16,22 +16,19 @@ module top1 (
   // output logic txclk, rxclk,
   // input  logic txready, rxready
 	input logic clk, nrst,
-	output logic zero, negative, regWrite, aluSrc, d_ready, i_ready, memWrite, memRead,
+	output logic zero, negative, regWrite, aluSrc, d_ready, i_ready, memWrite, memRead, Wen,busy_o, Ren,
 	output logic [3:0] aluOP,
 	output logic [4:0] regsel1, regsel2, w_reg,
 	output logic [5:0] cuOP,
 	output logic [19:0] imm,
-	output logic [31:0] memload, aluIn, aluOut, immOut, pc, writeData, regData1, regData2, instruction_out,
-  // output logic [31:0][31:0] test_memory ,
-  // output logic [31:0][31:0] test_nxt_memory 
+	output logic [31:0] memload, aluIn, aluOut, immOut, pc, writeData, regData1, regData2, instruction_out, ramaddr, ramload, ramstore
 );
-assign d_ready = 0;
+
 logic [31:0] instruction;
+
 mux aluMux(.in1(immOut), .in2(regData2), .en(aluSrc), .out(aluIn));
 
 alu arith(.aluOP(aluOP), .inputA(regData1), .inputB(aluIn), .ALUResult(aluOut), .zero(zero), .negative(negative));
-
-//request ru(.clk(clk), .nRST(nrst), .imemload(), .imemaddr(), .dmmaddr(), .dmmstore(), .ramaddr(), .ramload(), .ramstore(), .cuOP(), .Ren(), .Wen());
 
 register_file DUT(.clk(clk), .nRST(nrst), .reg_write(regWrite), .read_index1(regsel1), .read_index2(regsel2), 
 .read_data1(regData1), .read_data2(regData2), .write_index(w_reg), .write_data(writeData));
@@ -46,8 +43,13 @@ writeToReg write(.cuOP(cuOP), .memload(memload), .aluOut(aluOut), .imm(immOut), 
 
 signExtender signex(.imm(imm), .immOut(immOut), .CUOp(cuOP));
 
-//request ru(.clk(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc[5:0]), .dmmaddr(aluout[5:0]), .dmmstore(regData2), .ramaddr(), .ramload(), .ramstore(), .cuOP(), .Ren(), .Wen());
+ request ru(.CLK(clk), .nRST(nrst), .imemload(instruction), .imemaddr(pc), .dmmaddr(aluOut), .dmmstore(regData2), .ramaddr(ramaddr), .ramload(ramload), .ramstore(ramstore), 
+.cuOP(cuOP), .Wen(Wen), .busy_o(busy_o), .dmmload(memload), .Ren(Ren));
 
-ram ra(.clk(clk), .nRst(nrst), .write_enable(memWrite), .read_enable(1), .address_DM(aluOut[5:0]), .address_IM(pc[5:0]), .data_in(regData2), .data_out(memload), .instr_out(instruction), .pc_enable(i_ready), .CUOp(cuOP));
+
+ru_ram rram (.clk(clk), .nRst(nrst), .write_enable(write_enable), .addr(addr), .data_in(datain), .data_out(dataout), .busy(busy_o));
+
+//ram ra(.clk(clk), .nRst(nrst), .write_enable(memWrite), .read_enable(1), .address_DM(aluOut[5:0]), .address_IM(pc[5:0]), .data_in(regData2), .data_out(memload), .instr_out(instruction), .pc_enable(i_ready), .display(display));
+
 assign instruction_out = instruction;
 endmodule
