86ede9205bd18ddedb54bad6d60f5e49a8021a49
Weichih Hsieh <hsieh81@ee65lnx39.ecn.purdue.edu>
2024-07-09 13:20:54 -0400

    waveforms!

--- src/ru_ram.sv
+++ src/ru_ram.sv
@@ -1,59 +1,59 @@
-// // module ru_ram (
-// //     input logic clk, nRst, write_enable, 
-// //     input logic [31:0] addr,
-// //     input logic [31:0] data_in,
-// //     output logic [31:0] data_out,
-// //     output logic busy,
-// //     output logic [31:0] display
-// // );
+module ru_ram (
+    input logic clk, nRst, write_enable, 
+    input logic [31:0] addr,
+    input logic [31:0] data_in,
+    output logic [31:0] data_out,
+    output logic busy,
+    output logic [31:0] display
+);
 
-// // logic [31:0] memory [255:0];
-// // logic [31:0] nxt_memory [255:0];
+logic [31:0] memory [255:0];
+logic [31:0] nxt_memory [255:0];
 
-// // typedef enum logic {IDLE, WAIT} StateType;
-// // StateType state, next_state;
+typedef enum logic {IDLE, WAIT} StateType;
+StateType state, next_state;
 
-// // always_ff @(posedge clk, negedge nRst) begin
-// //     if (!nRst) begin
-// //         $readmemh("fill.mem", memory);
-// //         state <= IDLE;
-// //     end else begin
-// //         for (int i = 0;i<256 ;i++ ) begin
-// //             memory[i] <= nxt_memory[i];
-// //         end
-// //         state <= next_state;
-// //     end
-// // end
+always_ff @(posedge clk, negedge nRst) begin
+    if (!nRst) begin
+        $readmemh("fill.mem", memory);
+        state <= IDLE;
+    end else begin
+        for (int i = 0;i<256 ;i++ ) begin
+            memory[i] <= nxt_memory[i];
+        end
+        state <= next_state;
+    end
+end
 
-// // always_comb begin
-// //     next_state = state;
-// //     for (int i = 0;i<256 ;i++ ) begin
-// //         nxt_memory[i] = memory[i];
-// //     end
+always_comb begin
+    next_state = state;
+    for (int i = 0;i<256 ;i++ ) begin
+        nxt_memory[i] = memory[i];
+    end
 
-// //     busy = 0;
+    busy = 0;
 
-// //     case (state)
-// //         IDLE : begin
-// //             busy = 0;
-// //             // next_state = WAIT;
-// //             next_state = IDLE;
-// //             if (write_enable) begin
-// //                 nxt_memory[addr>>2] = data_in;
-// //             end 
-// //         end
+    case (state)
+        IDLE : begin
+            busy = 0;
+            // next_state = WAIT;
+            next_state = IDLE;
+            if (write_enable) begin
+                nxt_memory[addr>>2] = data_in;
+            end 
+        end
 
-// //         WAIT : begin
-// //             next_state = IDLE;
-// //             busy = 1;
-// //         end
-// //     endcase  
-// //     display = {memory [0]};
-// // end
+        WAIT : begin
+            next_state = IDLE;
+            busy = 1;
+        end
+    endcase  
+    display = {memory [0]};
+end
 
-// // assign data_out = memory[addr>>2];
+assign data_out = memory[addr>>2];
 
-// // endmodule
+endmodule
 
 
 
@@ -110,25 +110,25 @@
 
 // endmodule
 
-module ru_ram (din, addr, write_en, clk, dout);// 512x8
-parameter addr_width = 12;
-parameter data_width = 32;
-input [addr_width-1:0] addr;
-input [data_width-1:0] din;
-input write_en, clk;
-output [data_width-1:0] dout;
-reg [data_width-1:0] dout; // Register for output.
-reg [data_width-1:0] mem [(1<<addr_width)-1:0];
+// module ru_ram (din, addr, write_en, clk, dout);// 512x8
+// parameter addr_width = 12;
+// parameter data_width = 32;
+// input [addr_width-1:0] addr;
+// input [data_width-1:0] din;
+// input write_en, clk;
+// output [data_width-1:0] dout;
+// reg [data_width-1:0] dout; // Register for output.
+// reg [data_width-1:0] mem [(1<<addr_width)-1:0];
 
-initial begin
-    $readmemh("fill.mem", mem);
-end
+// initial begin
+//     $readmemh("fill.mem", mem);
+// end
 
-always @(posedge clk)begin
-if (write_en)
-    mem[(addr)] <= din;
-    dout = mem[addr]; // Output register controlled by clock.
-end
+// always @(posedge clk)begin
+// if (write_en)
+//     mem[(addr)] <= din;
+//     dout = mem[addr]; // Output register controlled by clock.
+// end
 
 
-endmodule
+// endmodule
